7150b46fe20b91a5b43430a8c52ba026d5cb4dca
Caleb McBride <atfbassman@yahoo.com>
2019-05-08 15:16:31 -0600

    finished problems1-5

--- practice.js
+++ practice.js
@@ -50,7 +50,11 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
   Loop through the given array and alert every item in the array.
 */
 
-//Code Here
+function looper(family) {
+  for (let i = 0; i < family.length; i++){
+    alert(family[i])
+  }
+}
 
 
 
@@ -65,7 +69,11 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
   Loop backwards, starting at the end of the letters array, alerting every item in the array.
 */
 
-//Code Here
+function reversedLooper(letters){
+  for (let i = letters.length - 1; i >=0; i--){
+    alert(letters[i])
+  }
+}
 
 
 
@@ -80,7 +88,15 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
   Return an array that contains the even numbers from the nums array.
 */
 
-//Code Here
+function evenFinder(nums) {
+  let evens = []
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] % 2 === 0) {
+      evens.push(nums[i])
+    }
+  }
+ return evens
+}
 
 
 
